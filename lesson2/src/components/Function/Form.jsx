0a1075079f277562fb0742d0a478c6ba386cacfc
Chansky-Gleb <mesiagutov_gleb@mail.ru>
2022-08-22 21:36:02 +0300

    Made homework3

--- lesson2/src/components/Function/Form.jsx
+++ lesson2/src/components/Function/Form.jsx
@@ -1,11 +1,20 @@
-import React,{useState, useEffect} from 'react'
-import {Input} from './Input'
-import { Button } from './Button'
-import '../../style.css'
+import React, { useState, useEffect, useRef } from 'react';
+import { Button, TextField, ThemeProvider, Box, createTheme } from '@mui/material';
+import { red } from '@mui/material/colors';
+import { ChatList } from './ChatList';
+import '../../style.css';
 
 const Form = () => {
     const [messageList, setMessageList] = useState([]);
     const [message, setMessage] = useState('');
+
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: red[700],
+            }
+        }
+    })
 
 
     const handleChange = (ev) => {
@@ -14,39 +23,69 @@ const Form = () => {
 
     const handleClick = () => {
         if (message) {
-            setMessageList([...messageList, { author: 'you', text: (message) }]);
+            setMessageList([...messageList, { author: 'You', text: (message) }]);
             setMessage('');
         } else {
             setMessageList(messageList);
         }
     }
 
+    const inputRef = useRef(null);
 
+    useEffect(() => {
+        inputRef.current.focus();
+    }, [])
 
     useEffect(() => {
         const lastEl = messageList[messageList.length - 1]
         if (messageList.length === 0) {
             setMessageList(messageList);
         } 
-        else if ( lastEl.author === 'you') {
+        else if ( lastEl.author === 'You') {
                 const timer = setTimeout(() => {
-                    setMessageList([...messageList, { author: 'bot', text: ('Hello') }]);
+                    setMessageList([...messageList, { author: 'Bot', text: ('Hello') }]);
                 return clearTimeout(timer);
                 }, 1500);
             }
     }, [messageList]);
     
     
-    return(
-        <> 
-            <ul> 
-            {messageList.map((message) =>
-                <li key={message.id}>{message.author}:{message.text}</li>)}
-        </ul >
-             
-            <Input change={handleChange} message={message} />
-        <Button click={handleClick} />
-        </>
+    return (
+        <ThemeProvider theme={theme}>
+            <div className='container'>
+                <ChatList/>
+                <Box component="form"
+                className='chat__box'
+                >
+                    <ul className='chat'> 
+                    {messageList.map((message, index) =>
+                        <li key={index}
+                            className='chat__item'>
+                            {message.author}: {message.text}
+                        </li>
+                    )}
+                    </ul >
+            
+                    <TextField
+                        className='chat__input'
+                        onChange={handleChange}
+                        value={message}
+                        id="outlined-basic"
+                        label="Message"
+                        variant="outlined"
+                        ref={inputRef}
+                        size="small"
+                        focused
+                    />
+                    <Button
+                        className='chat__button'
+                        onClick={handleClick}
+                        variant="contained">
+                        Send
+                    </Button>
+                </Box>
+            </div>
+        </ThemeProvider>
     )
        
 }
