52bc61c0f18d89b1e581cb2ff06644734b1888ee
Chansky-Gleb <mesiagutov_gleb@mail.ru>
2022-09-06 20:10:26 +0300

    Made homework8

---
+++ lesson8/src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getComments } from "./redux/commentsReducer/commentsReducer";
+
+function App() {
+
+  const comments = useSelector(state => state.comments);
+  const isLoading = useSelector(state => state.isLoading);
+  const isError = useSelector(state => state.isError);
+  const dispatch = useDispatch();
+
+  const handleRestart = () => {
+    dispatch(getComments());
+  }
+
+  useEffect(() => {
+    dispatch(getComments())
+  }, [])
+
+  if (isError) {
+    return (
+      <div>
+        <h1>Упс что-то пошло не так ...</h1>
+        <button onClick={handleRestart}>Обновить</button>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {isLoading ? (
+        <div>
+          <h1>Идет загрузка ...</h1>
+        </div>
+      ) : (
+          comments.map((comment) => {
+            return (
+            <div key={comment.id}>
+              {comment.name}
+            </div>
+            )
+          })
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ lesson8/src/redux/initialStore.jsx
@@ -0,0 +1,7 @@
+import { applyMiddleware, createStore } from "redux";
+import { commentsReducer } from "./commentsReducer/commentsReducer";
+import thunk from 'redux-thunk';
+
+ 
+
+export const store = createStore(commentsReducer, applyMiddleware(thunk));
