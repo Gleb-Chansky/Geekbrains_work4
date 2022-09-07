41533fff65b4f7649fafdb6b33667cd1f1834997
Chansky-Gleb <mesiagutov_gleb@mail.ru>
2022-09-07 16:09:07 +0300

    Made homework9

---
+++ lesson9/src/services/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import "firebase/compat/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDcMp_N2sycq7y_DOdzQV-LM7ynARzytz8",
+    authDomain: "geek-brains-lesson9.firebaseapp.com",
+    projectId: "geek-brains-lesson9",
+    storageBucket: "geek-brains-lesson9.appspot.com",
+    messagingSenderId: "488011579026",
+    appId: "1:488011579026:web:81fb484bb8a69c475bb04b"
+  };
+
+const firebaseDB = firebase.initializeApp(firebaseConfig);
+export const db = firebaseDB.database().ref()
+export const auth = firebase.auth();
